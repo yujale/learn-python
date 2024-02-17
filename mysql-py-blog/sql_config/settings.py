bcf398d91839f2698bf1ad3e57e547e60ea68654
yujiale@auhting.cn <yujiale@auhting.cn>
2024-02-17 11:18:51 +0800

    feat: blog 博客编写

---
+++ mysql-py-blog/sql_config/settings.py
@@ -0,0 +1,14 @@
+DB_POOL_CONN = {
+    "maxconnections": 5,
+    "mincached": 2,
+    "maxcached": 3,
+    "blocking": True,
+    "setsession": [],
+    "ping": 0,
+    "host": '47.109.31.55',
+    "port": 13306,
+    "user": 'root',
+    "password": 'rootroot',
+    "database": 'blog',
+    "charset": 'utf8'
+}
