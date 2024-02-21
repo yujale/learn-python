c380c68695739a769b4bf15620fcd9fc34d6b9bb
yujale98 <yujale98@gmail.com>
2024-02-21 17:35:12 +0800

    feat: 爬虫 selenium 示例

---
+++ crawler/03-数据解析/04-selenium/00- selenium 基本使用.py
@@ -0,0 +1,12 @@
+"""
+- selenium
+  - 是一种浏览器自动化的工具，所谓的自动化是指，我们可以通过代码的形式制定一系列的行为动作，然后执行代码，这些动作就会同步触发在浏览器中。
+
+### 环境安装
+
+- 下载安装selenium：pip install selenium
+- 下载浏览器驱动程序：
+  - http://chromedriver.storage.googleapis.com/index.html
+- 查看驱动和浏览器版本的映射关系：
+  - http://blog.csdn.net/huilan_same/article/details/51896672
+"""
---
+++ crawler/03-数据解析/04-selenium/06-selenium获取cookie.py
@@ -0,0 +1,14 @@
+from selenium import webdriver
+
+browser = webdriver.Chrome()
+browser.get('https://www.zhihu.com/explore')
+# 获取cookie
+cookies = browser.get_cookies()
+# 解析cookie
+dic = {}
+for cookie in cookies:
+    key = cookie['name']
+    value = cookie['value']
+    dic[key] = value
+print(dic)  # 在爬虫中可以使用的cookie
+browser.close()
